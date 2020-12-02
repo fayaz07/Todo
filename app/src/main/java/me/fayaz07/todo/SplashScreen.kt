271d7f28124716df109c029018b9bbff4c519f1c
Mohammad Fayaz <fayazfz07@gmail.com>
2020-12-02 22:27:04 +0530

    setup splash screen and icons

--- app/src/main/java/me/fayaz07/todo/SplashScreen.kt
+++ app/src/main/java/me/fayaz07/todo/SplashScreen.kt
@@ -3,9 +3,11 @@ package me.fayaz07.todo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
     }
 }
